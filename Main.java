87bc2fd0460d2812e679b63d1282b0a4a2532c96
Eduardo BA <133010820+SalelaDudu@users.noreply.github.com>
2024-09-13 14:44:11 -0300

    Inicio do projeto

---
+++ Main.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+public class Main {
+    public static void main(String[] args) {
+        JFrame janela = new JFrame("bla");
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setSize(480,360);
+        janela.setVisible(true);
+    }
+}
